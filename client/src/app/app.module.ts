eac838fe3c75579568ed4cd2172f69e6365f45ab
davidKwon89 <tienne89@gmail.com>
2017-07-25 13:41:20 +0900

    sass로 변경

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { MdToolbarModule, MdIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
