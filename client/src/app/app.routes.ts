422ef061dba7e987fc009c6ce4dfb4119200766f
davidKwon89 <tienne89@gmail.com>
2017-08-01 17:38:22 +0900

    sass -> scss 로 변경

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component'
 
 /**
  * Define app module routes here, e.g., to lazily load a module
  * (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
  */
 export const AppRoutes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'resume' },
-  { path: 'resume', loadChildren: 'app/resume/resume.module#ResumeModule' }
+
+  { path: '', component: LayoutComponent, children: [
+    {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'resume'
+    },
+    {
+      path: 'resume',
+      loadChildren: 'app/resume/resume.module#ResumeModule'
+    }
+  ]},
 ];
--- client/src/app/core/core.module.tns.ts
+++ client/src/app/core/core.module.tns.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 
 import { SHARED_MODULES, COMPONENT_DECLARATIONS, COMPONENT_EXPORTS } from './core.common';
-import { MaterialComponentsModule } from '../common/material-components.module';
 
 @NgModule({
   imports: [
-    MaterialComponentsModule,
     ...SHARED_MODULES,
   ],
   declarations: [
-    ...COMPONENT_DECLARATIONS,
+    ...COMPONENT_DECLARATIONS
   ],
   exports: [
     ...COMPONENT_EXPORTS
