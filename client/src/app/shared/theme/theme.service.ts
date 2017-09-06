b76fb3892f8ac84cc690a4e6449fae99dc501ce7
tienne <tienne89@gmail.com>
2017-09-06 23:05:37 +0900

    테마 서비스 shared로 변경중

--- client/src/app/shared/theme/theme.service.ts
+++ client/src/app/shared/theme/theme.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ITheme } from './theme.model';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import {ReplaySubject} from 'rxjs/ReplaySubject';
 export { ITheme } from './theme.model';
 
 @Injectable()
 export class ThemeService {
+  private currentTheme: ITheme;
+  currentThemeSubject: ReplaySubject<ITheme> = new ReplaySubject();
+  currentTheme$ = this.currentThemeSubject.asObservable();
+
   private _themes: ITheme[] = [
     {
       primary: '#e91e63',
@@ -31,6 +38,10 @@ export class ThemeService {
       isDark: false
     }
   ];
+
+  // 테마 리스트 옵저블
+  themesSubject: BehaviorSubject<ITheme[]> = new BehaviorSubject(this._themes);
+  themes$: Observable<ITheme[]> = this.themesSubject.asObservable();
 
   constructor() { }
 
