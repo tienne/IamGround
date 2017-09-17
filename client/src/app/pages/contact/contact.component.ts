4472891709a05d1a3597c6cac3b1d7cc18949626
tienne <tienne89@gmail.com>
2017-09-17 18:43:11 +0900

    카카오링크 기능 추가

--- client/src/app/pages/contact/contact.component.ts
+++ client/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {KakaoLinkService} from '../../shared/kakao-link/kakao-link.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _kakao: KakaoLinkService
+  ) { }
 
   ngOnInit() {
   }
 
   getSocialLogo(social: string) {
     return `assets/images/contact-logos/${social}.png`;
+  }
+
+  kakao() {
+    this._kakao.sendLinkDefault();
   }
 }
--- client/src/app/shared/kakao-link/kakao-link.service.ts
+++ client/src/app/shared/kakao-link/kakao-link.service.ts
@@ -1,15 +1,85 @@
 import { Injectable, Optional } from '@angular/core';
 import { KakaoLinkConfig } from './kakao-link-config';
+import {AsyncSubject} from 'rxjs/AsyncSubject';
+import {Observable} from 'rxjs/Observable';
+import {WindowRef} from '../window-ref';
+import 'rxjs/add/operator/combineLatest';
+import 'rxjs/add/operator/take';
+import {MdSnackBar} from '@angular/material';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class KakaoLinkService {
   private _kakao;
+  private loadSubject: AsyncSubject<boolean> = new AsyncSubject();
+  private load$: Observable<boolean> = this.loadSubject.asObservable();
+  private _window;
 
-  constructor(@Optional() config: KakaoLinkConfig) {
-    this.loadKakaoLinkScript(config);
+  constructor(
+    @Optional() config: KakaoLinkConfig,
+    _window: WindowRef,
+    private _snackBar: MdSnackBar
+  ) {
+    this._window = _window.nativeWindow;
+    this.kakaoLoad();
+
+    this.load$.take(1).subscribe(() => {
+      this._kakao = this._window.Kakao;
+      if (!this._kakao.API) {
+        this._kakao.init(config.key);
+      }
+    });
   }
 
-  private loadKakaoLinkScript(config: KakaoLinkConfig) {
+  private kakaoLoad() {
+    if (!this._window.Kakao) {
+      let kakaoScript = false;
+      const scripts = this._window.document.querySelectorAll('script[src]');
+      const match = /developers\.kakao\.com\/sdk\/js\/kakao/;
 
+      scripts.forEach(script => {
+        if (match.test(script.src)) {
+          kakaoScript = true;
+        }
+      });
+
+      if (!kakaoScript) {
+        const loadScript$ = this.loadKakaoLinkScript();
+        this.load$ = this.load$.combineLatest(loadScript$);
+      }
+    }
+
+    this.loadSubject.next(true);
+    this.loadSubject.complete();
+  }
+  private loadKakaoLinkScript() {
+    let scriptLoad$;
+    const sdkUrl = '//developers.kakao.com/sdk/js/kakao.js',
+      script = this._window.document.createElement('script');
+
+    script.src = sdkUrl;
+    scriptLoad$ = Observable.fromEvent(script, 'load');
+
+    this._window.document.getElementsByTagName('body')[0].appendChild(script);
+    return scriptLoad$;
+  }
+
+  sendLinkDefault() {
+    if (this._window.orientation !== undefined || !environment.production) {
+      this._kakao.Link.sendTalkLink({
+        label : '개발자 권윤학 - I am Ground 자기소개하기',
+        image: {
+          src: 'https://tienne.github.io/assets/images/avatar.png',
+          width: 90,
+          height : 90
+        },
+        webButton: {
+          text: '웹으로 보기',
+          url: 'https://tienne.github.io'
+        }
+      });
+    } else {
+      this._snackBar.open('모바일 디바이스에서만 가능합니다.', '', {duration: 2000});
+    }
   }
 }
